e86ab8e4eebdaf02426e54e5978a19f791fea187
Bhazi <109984736+Bhazi@users.noreply.github.com>
2022-12-19 15:53:18 +0530

    queryselector and queryselectorAll used

--- htmlProject/main.js
+++ htmlProject/main.js
@@ -1,4 +1,13 @@
-var p = document.getElementsByTagName("li");
-for (let i = 0; i < p.length; i++) {
-  p[i].style.fontWeight = "bold";
+var item2 = document.querySelector("#item2");
+item2.style.backgroundColor = "green";
+var item3 = document.querySelector("#item3");
+item3.style.display = "none";
+
+var items = document.querySelectorAll(".list-group-item");
+items[1].style.color = "green";
+
+for (let i = 0; i <= items.length; i++) {
+  if (i % 2 == 0) {
+    items[i].style.backgroundColor = "green";
+  }
 }
