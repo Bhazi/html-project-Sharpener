0f9c1af7ec7e5022b875b31428ecf73017d3a8c7
Bhazi <109984736+Bhazi@users.noreply.github.com>
2022-12-19 14:43:41 +0530

    added Green Color,make it bold

--- htmlProject/main.js
+++ htmlProject/main.js
@@ -1,2 +1,7 @@
-document.getElementById("header-title").textContent = "hello";
-HTMLObjectElement.apply;
+var p = document.getElementsByClassName("list-group-item");
+p[2].style.backgroundColor = "green";
+
+for (let i = 0; i < p.length; i++) {
+    p[i].style.fontWeight = "bold"
+    
+}
