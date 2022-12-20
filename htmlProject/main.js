5d31a27f6ceafca1d38bd59aa4ef5c2fd9367486
Bhazi <109984736+Bhazi@users.noreply.github.com>
2022-12-20 23:37:38 +0530

    adding values to  local storage as a object model.

--- htmlProject/main.js
+++ htmlProject/main.js
@@ -1,8 +1,15 @@
 var tyt = document.getElementById("addForm");
-tyt.addEventListener('submit',rtytty);
+tyt.addEventListener("submit", rtytty);
 
-function rtytty(e){
+function rtytty(e) {
   e.preventDefault();
   var textItem = document.getElementById("item").value;
-  localStorage.setItem('name',textItem)
+  var descriptionVal = document.getElementById("description").value;
+
+  let object = {
+    name: textItem,
+    description: descriptionVal,
+  };
+  var tt = JSON.stringify(object);
+  localStorage.setItem("asa", tt);
 }
