72d9ba460ff89861471a6cace619da2facc04420
Bhazi <109984736+Bhazi@users.noreply.github.com>
2022-12-21 15:50:47 +0530

    Added edit and delete button with mul L.Stoarge

--- htmlProject/main.js
+++ htmlProject/main.js
@@ -1,15 +1,39 @@
-var tyt = document.getElementById("addForm");
-tyt.addEventListener("submit", rtytty);
+var form = document.getElementById("addForm");
+form.addEventListener("click", adda);
 
-function rtytty(e) {
-  e.preventDefault();
-  var textItem = document.getElementById("item").value;
-  var descriptionVal = document.getElementById("description").value;
+var co = 0;
+function adda(e) {
+  if (e.target.classList.contains("submit")) {
+    e.preventDefault();
+    var textItem = document.getElementById("name").value;
+    var textPhone = document.getElementById("phoneNo").value;
 
-  let object = {
-    name: textItem,
-    description: descriptionVal,
-  };
-  var tt = JSON.stringify(object);
-  localStorage.setItem("asa", tt);
+    let object = {
+      name: textItem,
+      phoneNo: textPhone,
+    };
+    var val = JSON.stringify(object);
+    localStorage.setItem(co++, val);
+
+    // showing values
+    var valueName = document.createElement("p");
+    valueName.id = "newName";
+    valueName.style.marginLeft = "120px";
+    valueName.textContent = textItem + " " + textPhone;
+    form.appendChild(valueName);
+
+    var edit = document.createElement("button");
+    edit.id = "edit";
+    edit.appendChild(document.createTextNode("EDIT"));
+    edit.className = "btn btn-sm edit";
+    edit.style.marginLeft = "26px";
+    valueName.appendChild(edit);
+
+    var edit = document.createElement("button");
+    edit.id = "delete";
+    edit.appendChild(document.createTextNode("Delete"));
+    edit.className = "btn btn-danger btn-sm delete";
+    edit.style.marginLeft = "26px";
+    valueName.appendChild(edit);
+  }
 }
